e27a7e07fcc632d4ec83a5a516fc607c1e503457
Emran Hossain <emranffl4@gmail.com>
2023-10-09 04:45:15 +0600

    fix(index.ts): variable rename

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ type KeyMapping<T> = Record<keyof T, string>
 
 type DeepKeyMapping<T> = {
   [K in keyof T]: T[K] extends object
-  ? T[K] extends Array<NonNullable<unknown>>
-  ? Array<DeepKeyMapping<T[K][number]>>
-  : DeepKeyMapping<T[K]>
-  : T[K]
+    ? T[K] extends Array<NonNullable<unknown>>
+      ? Array<DeepKeyMapping<T[K][number]>>
+      : DeepKeyMapping<T[K]>
+    : T[K]
 }
 
 /**
@@ -13,7 +13,7 @@ type DeepKeyMapping<T> = {
  *
  * @param inputObject - The input object to be parsed.
  * @param keyMapping - An object specifying key mapping rules.
- * @example 
+ * @example
  * ```ts
  * const inputObject = {
  *  foo: "bar",
@@ -21,14 +21,14 @@ type DeepKeyMapping<T> = {
  *   qux: "quux"
  *   }
  * }
- * 
+ *
  * const keyMapping = {
  *  foo: "boo",
  *  qux: "que"
  * }
- * 
+ *
  * const parsedObject = deepObjectKeyAlternator(inputObject, keyMapping)
- * 
+ *
  * console.log(parsedObject)
  * // {
  * //  boo: "bar",
@@ -37,37 +37,46 @@ type DeepKeyMapping<T> = {
  * //   }
  * // }
  * ```
- * 
+ *
  * @returns A new object with keys renamed according to the key mapping.
  */
 export function deepObjectKeyAlternator<T extends Record<string, any>>(
   inputObject: T,
-  keyMapping: Partial<KeyMapping<DeepKeyMapping<T>>> | Record<string, string> = {}
+  keyMapping:
+    | Partial<KeyMapping<DeepKeyMapping<T>>>
+    | Record<string, string> = {}
 ): DeepKeyMapping<T> {
-  const parsedObject = {} as DeepKeyMapping<T>
+  const alteredObject = {} as DeepKeyMapping<T>
 
   for (const key in inputObject) {
     if (Object.prototype.hasOwnProperty.call(inputObject, key)) {
       const newKey = keyMapping[key] || key
 
       if (typeof newKey === "string") {
-        if (typeof inputObject[key as keyof T] === "object" && inputObject[key as keyof T] !== null) {
+        if (
+          typeof inputObject[key as keyof T] === "object" &&
+          inputObject[key as keyof T] !== null
+        ) {
           if (Array.isArray(inputObject[key as keyof T])) {
-            parsedObject[newKey as keyof DeepKeyMapping<T>] = inputObject[key as keyof T].map(
-              (item: T[keyof T][number]) =>
-                deepObjectKeyAlternator(
-                  item,
-                  keyMapping as Partial<KeyMapping<DeepKeyMapping<T[keyof T][number]>>>
-                )
+            alteredObject[newKey as keyof DeepKeyMapping<T>] = inputObject[
+              key as keyof T
+            ].map((item: T[keyof T][number]) =>
+              deepObjectKeyAlternator(
+                item,
+                keyMapping as Partial<
+                  KeyMapping<DeepKeyMapping<T[keyof T][number]>>
+                >
+              )
             ) as DeepKeyMapping<T>[keyof T]
           } else {
-            parsedObject[newKey as keyof DeepKeyMapping<T>] = deepObjectKeyAlternator(
-              inputObject[key as keyof T],
-              keyMapping as Partial<KeyMapping<DeepKeyMapping<T[keyof T]>>>
-            ) as DeepKeyMapping<T>[keyof T]
+            alteredObject[newKey as keyof DeepKeyMapping<T>] =
+              deepObjectKeyAlternator(
+                inputObject[key as keyof T],
+                keyMapping as Partial<KeyMapping<DeepKeyMapping<T[keyof T]>>>
+              ) as DeepKeyMapping<T>[keyof T]
           }
         } else {
-          parsedObject[newKey as keyof DeepKeyMapping<T>] = inputObject[
+          alteredObject[newKey as keyof DeepKeyMapping<T>] = inputObject[
             key as keyof T
           ] as DeepKeyMapping<T>[keyof T]
         }
@@ -75,5 +84,5 @@ export function deepObjectKeyAlternator<T extends Record<string, any>>(
     }
   }
 
-  return parsedObject
+  return alteredObject
 }
